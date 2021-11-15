3358a71c43e559655158efefcf075dfff8f895b5
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-11-15 17:10:05 +0100

    creata istanza vue e v-for per popolare il container chat

--- js/main.js
+++ js/main.js
@@ -1,1 +1,98 @@
-console.log('hey there');
+var app = new Vue(
+    {
+        el: '#container-app',
+        data: {
+            contacts:[
+                {
+                    nome: 'Michele',
+                    avatar:'_1',
+                    visibile: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Ricordati di dargli da mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            text: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    nome: 'Fabio',
+                    avatar:'_2',
+                    visibile: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            text: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            text: 'Bene grazie! Stasera cci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:22',
+                            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'send'
+                        }
+                    ],
+                },
+                {
+                    nome: 'Samuele',
+                    avatar:'_3',
+                    visibile: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            text: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            text: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            text: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    nome: 'Luisa',
+                    avatar:'_4',
+                    visibile: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        },
+                    ],
+                },
+            ]
+
+        },
+        medthods: {
+            prova(){
+                console.log(this.contacts.name);
+            }
+        }
+    }
+);
